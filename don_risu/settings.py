5c0cbcb8b41fcd3a5c8437280c017039552289ea
Kyle Cheon <kcheon@my.bcit.ca>
2025-04-08 13:52:23 -0700

    fix: fix settings to perceive secrets

--- don_risu/settings.py
+++ don_risu/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+
+def get_env_filename():
+    return os.environ.get('ENV_FILENAME', '.env')
+
+# Import env variables
+load_dotenv(find_dotenv(get_env_filename()))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
